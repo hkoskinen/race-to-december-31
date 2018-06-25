db7d9b6fc57cd28f7429a292473a06ac3013da3a
Henri Koskinen <hkoskinen@protonmail.com>
2018-06-25 21:41:12 +0300

    Add SingleDatePicker component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import { SingleDatePicker } from 'react-dates';
+import moment from 'moment';
+
+import 'react-dates/lib/css/_datepicker.css';
+import 'react-dates/initialize';
+
 import './App.css';
 
 class App extends Component {
+  state = {
+    date: moment(new Date((new Date()).getFullYear(), 0, 1)),
+    focused: false
+  }
   render() {
     return (
       <div className="App">
         <h1>Race to December 31</h1>
+
+        <SingleDatePicker
+          date={this.state.date}
+          onDateChange={date => this.setState({date})}
+          onFocusChange={({focused}) => this.setState({focused})}
+          focused={this.state.focused}
+          numberOfMonths={1}
+          hideKeyboardShortcutsPanel={true}
+        />
       </div>
     );
   }
